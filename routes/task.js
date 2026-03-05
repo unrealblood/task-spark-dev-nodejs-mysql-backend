1f8f4c1dcc8aa38d018b4cd1aa9ee2b332ed0f0e
Shubham Singh <shubham110084@outlook.com>
2026-03-05 10:29:39 +0530

    remove duplicate add task route

--- routes/task.js
+++ routes/task.js
@@ -45,27 +45,6 @@ tasksRouter.post("/add-task", async (req, res) => {
     }
 });
 
-tasksRouter.post("/add-task", async (req, res) => {
-    const { content, completed, userId } = req.body;
-
-    try {
-        const db = await getMYSQLClient();
-
-        await db.execute("CREATE TABLE IF NOT EXISTS tasks (_id INT AUTO_INCREMENT PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
-
-        const result = await db.execute("INSERT INTO tasks (content, completed, userId) VALUES (?, ?, ?)", [content, completed, userId]);
-
-        if(result) {
-            return res.json({success: true, taskId: result[0].insertId});
-        }
-
-        return res.json({success: false});
-    }
-    catch(error) {
-        return res.json({success: false, error: "Failed to add task in database. Error: " + error.message});
-    }
-});
-
 tasksRouter.put("/update-task-content/:taskId", async (req, res) => {
     const { taskId } = req.params;
     const { newContent } = req.body;
