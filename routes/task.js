f1876a5581fd32bf729a32d7e9af0e72677117a4
Shubham Singh <shubham110084@outlook.com>
2026-03-05 10:27:57 +0530

    add update task and delete task routes

--- routes/task.js
+++ routes/task.js
@@ -44,3 +44,89 @@ tasksRouter.post("/add-task", async (req, res) => {
         return res.json({success: false, error: "Failed to add task in database. Error: " + error.message});
     }
 });
+
+tasksRouter.post("/add-task", async (req, res) => {
+    const { content, completed, userId } = req.body;
+
+    try {
+        const db = await getMYSQLClient();
+
+        await db.execute("CREATE TABLE IF NOT EXISTS tasks (_id INT AUTO_INCREMENT PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await db.execute("INSERT INTO tasks (content, completed, userId) VALUES (?, ?, ?)", [content, completed, userId]);
+
+        if(result) {
+            return res.json({success: true, taskId: result[0].insertId});
+        }
+
+        return res.json({success: false});
+    }
+    catch(error) {
+        return res.json({success: false, error: "Failed to add task in database. Error: " + error.message});
+    }
+});
+
+tasksRouter.put("/update-task-content/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+    const { newContent } = req.body;
+
+    try {
+        const db = await getMYSQLClient();
+
+        await db.execute("CREATE TABLE IF NOT EXISTS tasks (_id INT AUTO_INCREMENT PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await db.execute("UPDATE tasks SET content = ? WHERE _id = ?", [newContent, taskId]);
+
+        if(result[0].affectedRows > 0) {
+            return res.json({success: true});
+        }
+
+        return res.json({success: false});
+    }
+    catch(ex) {
+        return res.json({success: false, error: "Failed to update task in database. Error: " + ex.message});
+    }
+});
+
+tasksRouter.put("/update-task-completed/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+    const { newCompleted } = req.body;
+
+    try {
+        const db = await getMYSQLClient();
+
+        await db.execute("CREATE TABLE IF NOT EXISTS tasks (_id INT AUTO_INCREMENT PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await db.execute("UPDATE tasks SET completed = ? WHERE _id = ?", [newCompleted, taskId]);
+
+        if(result[0].affectedRows > 0) {
+            return res.json({success: true});
+        }
+
+        return res.json({success: false});
+    }
+    catch(ex) {
+        return res.json({success: false, error: "Failed to update task in database. Error: " + ex.message});
+    }
+});
+
+tasksRouter.delete("/delete-task/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+
+    try {
+        const db = await getMYSQLClient();
+
+        await db.execute("CREATE TABLE IF NOT EXISTS tasks (_id INT AUTO_INCREMENT PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await db.execute("DELETE FROM tasks WHERE _id = ?", [taskId]);
+
+        if(result[0].affectedRows > 0) {
+            return res.json({success: true});
+        }
+
+        return res.json({success: false});
+    }
+    catch(ex) {
+        return res.json({success: false, error: "Failed to delete task in database. Error: " + ex.message});
+    }
+});
